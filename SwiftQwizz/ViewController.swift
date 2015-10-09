ee122eee1072a222850860dfaf8eee93611ce290
Alberto-Vega <el_vega@live.com>
2015-10-08 19:19:14 -0700

    Initial Commit

---
+++ SwiftQwizz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftQwizz
+//
+//  Created by Alberto Vega Gonzalez on 10/8/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
