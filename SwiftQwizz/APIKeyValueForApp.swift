446fac109303fbf39e546ac17aff024c85b27111
Alberto-Vega <el_vega@live.com>
2016-06-16 16:14:39 -0700

    Added FlurryAnalytics and gitignore

---
+++ SwiftQwizz/APIKeyValueForApp.swift
@@ -0,0 +1,9 @@
+//
+//  APIKeyValueForApp.swift
+//  SwiftQwizz
+//
+//  Created by Alberto Vega Gonzalez on 6/16/16.
+//  Copyright © 2016 Alberto Vega Gonzalez. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftQwizz/SwiftQwizz-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SwiftQwizz-Bridging-Header.h
+//  SwiftQwizz
+//
+//  Created by Alberto Vega Gonzalez on 6/16/16.
+//  Copyright © 2016 Alberto Vega Gonzalez. All rights reserved.
+//
+
+#ifndef SwiftQwizz_Bridging_Header_h
+#define SwiftQwizz_Bridging_Header_h
+
+
+#endif /* SwiftQwizz_Bridging_Header_h */
